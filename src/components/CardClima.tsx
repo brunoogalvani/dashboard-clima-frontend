cb94271a1254fd6b1fd263dcf5436a12aaae22ee
aqthurn <arthurnabas17@gmail.com>
2026-01-14 17:42:36 -0300

    feat: alteracao tsconfig.app.json para deploy

--- src/components/CardClima.tsx
+++ src/components/CardClima.tsx
@@ -1,5 +1,6 @@
 import { SquareArrowOutUpRight } from "lucide-react";
 import type { Clima } from "../types/apiTypes";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   clima: Clima;
@@ -8,6 +9,7 @@ interface Props {
 }
 
 export default function CardClima({ clima, showExpand, onExpand }: Props) {
+  const { t } = useTranslation();
   return (
       <div className="bg-linear-to-br from-white to-emerald-50 rounded-2xl p-4 border border-emerald-200 shadow-sm hover:shadow-lg hover:scale-103 hover:-translate-y-1 transition-all duration-300 animate-fade-in animation-delay-100">
         {showExpand && (
