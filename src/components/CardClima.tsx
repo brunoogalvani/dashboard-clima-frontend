3442208c48f8d36d7c573bb715bb0e86a0680606
aqthurn <arthurnabas17@gmail.com>
2026-01-14 17:52:34 -0300

    feat: resolvendo erro de deploy

--- src/components/CardClima.tsx
+++ src/components/CardClima.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function CardClima({ clima, showExpand, onExpand }: Props) {
-  const { t } = useTranslation();
+  const {} = useTranslation();
   return (
       <div className="bg-linear-to-br from-white to-emerald-50 rounded-2xl p-4 border border-emerald-200 shadow-sm hover:shadow-lg hover:scale-103 hover:-translate-y-1 transition-all duration-300 animate-fade-in animation-delay-100">
         {showExpand && (
