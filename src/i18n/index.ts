fbc546622919096bdf2ddb483b3f6486bb57cba1
aqthurn <arthurnabas17@gmail.com>
2026-01-14 17:43:42 -0300

    feat: alteracao index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import ptBR from "./locales/pt-br.json";
-import enUS from "./locales/en-us.json";
+import ptBR from "./locales/pt-BR.json";
+import enUS from "./locales/en-US.json";
 
 i18n.use(initReactI18next).init({
   resources: {
